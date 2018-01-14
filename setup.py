1f236314b5b32545c57e072a8f6c42e009629c39
Anton Jouline <anton.jouline@cbsinteractive.com>
2018-01-14 00:27:44 -0800

    corrections for py2exe setup

--- setup.py
+++ setup.py
@@ -26,14 +26,35 @@ class Target:
 manifest_template = '''
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
-<assemblyIdentity
+  <assemblyIdentity
     version="5.0.0.0"
     processorArchitecture="x86"
     name="%(prog)s"
     type="win32"
-/>
-<description>%(prog)s Program</description>
-<dependency>
+  />
+  <description>%(prog)s</description>
+  <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+    <security>
+      <requestedPrivileges>
+        <requestedExecutionLevel
+            level="asInvoker"
+            uiAccess="false">
+        </requestedExecutionLevel>
+      </requestedPrivileges>
+    </security>
+  </trustInfo>
+  <dependency>
+    <dependentAssembly>
+      <assemblyIdentity
+            type="win32"
+            name="Microsoft.VC90.CRT"
+            version="9.0.21022.8"
+            processorArchitecture="x86"
+            publicKeyToken="1fc8b3b9a1e18e3b">
+      </assemblyIdentity>
+    </dependentAssembly>
+  </dependency>
+  <dependency>
     <dependentAssembly>
         <assemblyIdentity
             type="win32"
@@ -44,7 +65,7 @@ manifest_template = '''
             language="*"
         />
     </dependentAssembly>
-</dependency>
+  </dependency>
 </assembly>
 '''
 
@@ -67,8 +88,13 @@ setup(
         data_files=[
             (".",[
                 "default.png","shorts-mask.png","shorts63-mask.png",
-                "wizard.png","converter.txt"]),
+                "wizard.png","converter.txt","msvcr90.dll"]),
             ("docs",[
                 "docs/README.txt"])
-        ]
+        ],
+        options={
+            "py2exe": {
+                "dll_excludes": ["MSVCP90.DLL"],
+             }
+        },
 )
